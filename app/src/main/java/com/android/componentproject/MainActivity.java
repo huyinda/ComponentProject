6780f5f6877e8b2ed2e331cf83bf61fd2860dbb5
huyinda2019 <915738429@qq.com>
2020-03-05 17:26:15 +0800

    新增业务模块的application

--- app/src/main/java/com/android/componentproject/MainActivity.java
+++ app/src/main/java/com/android/componentproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.componentproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
